58c88918c55b1288478ae7402a0060b5d0f4ad34
Your Name <you@example.com>
2025-08-29 23:53:31 +0600

    before 4.13

--- models/note.js
+++ models/note.js
@@ -1,20 +1,4 @@
-const config = require('../utils/config')
 const mongoose = require('mongoose')
-
-mongoose.set('strictQuery', false)
-
-const url = config.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose
-  .connect(url)
-
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
